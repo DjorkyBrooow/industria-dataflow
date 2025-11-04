eeb1e6252e140cbc91af80cc4530702e7eed3e95
DjorkyBrooow <106020416+DjorkyBrooow@users.noreply.github.com>
2025-11-04 14:51:32 +0100

    Modification api

--- model_service/app.py
+++ model_service/app.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 import joblib
 import os
 import pandas as pd
+import subprocess
 
 
 MODEL_PATH = os.environ.get('MODEL_PATH','./model.pkl')
@@ -16,20 +17,20 @@ class InputRow(BaseModel):
     flow: float
 
 
-@app.on_event('startup')
-def load_model():
-    global model
-    try:
-        model = joblib.load(MODEL_PATH)
-        print('Model loaded')
-    except Exception as e:
-        print('Model load error', e)
-
-
 @app.post('/predict')
 def predict(row: InputRow):
     if model is None:
-        return {"error":"model not loaded"}
+        subprocess.run(["python3", "train_model.py"])
+        def load_model():
+            global model
+            try:
+                model = joblib.load(MODEL_PATH)
+                print('Model loaded')
+            except Exception as e:
+                print('Model load error', e)
+        load_model()
+        if model is None :
+            return {"error":"model not loaded"}
     X = pd.DataFrame([row.dict()])
     pred = model.predict(X)[0]
     return {"yield": float(pred)}
